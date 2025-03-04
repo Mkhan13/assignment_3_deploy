75832bd8ec42bf9e9a73c1e7bc5847e34bb87e0f
Mariam Khan <41304956+Mkhan13@users.noreply.github.com>
2025-03-04 16:26:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,4 @@ def search_papers():
 
 # Run the Flask app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
